a6c2729fb9db29edb9acb9e415ae0c143cf60d38
GabrielSoo <gabrielswk2001@gmail.com>
2023-05-10 02:33:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/comp4521_fitness_app/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.example.comp4521_fitness_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import com.example.comp4521_fitness_app.R;
+
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner mSpinnerRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mSpinnerRedirect = findViewById(R.id.spinner_redirect);
+        mSpinnerRedirect.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String redirectOption = adapterView.getItemAtPosition(position).toString();
+        Intent intent;
+
+        switch (redirectOption) {
+            case "Nutrition management":
+                intent = new Intent(this, NutritionManagementActivity.class);
+                startActivity(intent);
+                break;
+            case "Weight management":
+                intent = new Intent(this, WeightManagementActivity.class);
+                startActivity(intent);
+                break;
+            case "Fitness management":
+                intent = new Intent(this, FitnessManagementActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // Do nothing
+    }
+}
---
+++ app/src/main/java/com/example/comp4521_fitness_app/NutritionManagementActivity.java
@@ -0,0 +1,58 @@
+package com.example.comp4521_fitness_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import com.example.comp4521_fitness_app.R;
+
+import java.util.Arrays;
+
+public class NutritionManagementActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner mSpinnerRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_management);
+
+        mSpinnerRedirect = findViewById(R.id.spinner_redirect);
+        mSpinnerRedirect.setOnItemSelectedListener(this);
+
+        // Set the default selection to "Weight management"
+        String[] redirectOptions = getResources().getStringArray(R.array.redirect_options);
+        int weightManagementIndex = Arrays.asList(redirectOptions).indexOf("Nutrition management");
+        mSpinnerRedirect.setSelection(weightManagementIndex);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String redirectOption = adapterView.getItemAtPosition(position).toString();
+        Intent intent;
+
+        switch (redirectOption) {
+            case "Home Page":
+                intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case "Weight management":
+                intent = new Intent(this, WeightManagementActivity.class);
+                startActivity(intent);
+                break;
+            case "Fitness management":
+                intent = new Intent(this, FitnessManagementActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // Do nothing
+    }
+}
